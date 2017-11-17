22cadea69f6a27464be3148ae434a618b7bda72a
Eydis Embla Ludviksdottir <eydisembla@my.uri.edu>
2017-11-17 13:00:42 -0500

    modified is home function

--- car.cpp
+++ car.cpp
@@ -63,10 +63,11 @@ bool Car::check_distance(std::vector<int> pickup, std::vector<int> dropoff)
 
 bool Car::is_home() 
 {
+    if (current_loc == {0, 0}) return true;
     return false;
 }
 
-bool Car::has_passengers() 
+bool Car::has_passengers()
 {
     return false;
 }
